a0896d0ebb3164f554ee2bd45ff8f365d7c864b9
surveshoeb <surveshoeb@gmail.com>
2017-11-21 14:47:52 +0530

    Added TextInputLayout

---
+++ src/main/java/com/adevole/customresources/CutstomTextInputLayout.java
@@ -0,0 +1,10 @@
+package com.adevole.customresources;
+
+import android.support.design.widget.TextInputLayout;
+
+/**
+ * Created by surve on 21-Nov-17.
+ */
+
+public class CutstomTextInputLayout extends TextInputLayout {
+}
